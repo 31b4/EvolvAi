85f26e901938103550708b8506f614e213cc403b
JLoloCkik <jankai.lorinc@ckik.hu>
2024-12-24 23:22:34 +0100

    update gemfile add varibles and update manifest.js

--- Gemfile
+++ Gemfile
@@ -24,7 +24,8 @@ gem 'sass', '~> 3.7', '>= 3.7.4'
 # gem "bcrypt", "~> 3.1.7"
 
 # Sass compiler
-gem 'sassc-rails'
+gem 'sass-rails', '~> 6.0'
+
 
 # NoMethodError fix???
 gem 'sprockets-rails'
--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,7 @@
-   //= link_tree ../images
-   //= link_directory ../javascripts .js
-   //= link_directory ../stylesheets .css
+//= link_tree ../images
 
-   //= link application.js
-   //= link controllers/application.js
-   //= link controllers/hello_controller.js
-   //= link controllers/index.js
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link 'application.css'
