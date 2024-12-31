83a446bc586318de437793062fdb3e8ba80d05e9
JLoloCkik <jankai.lorinc@ckik.hu>
2024-12-31 13:35:04 +0100

    add devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,7 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, only: :index
+
   def index
-    @current_user = current_user
-    if @current_user
-      render 'home/index'
-    else
-      redirect_to login_path, notice: 'Please log in to continue'
-    end
-  end
 
-  private
-
-  def current_user
-    @current_user ||= AccessControl::User.find_by(id: session[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'sign_in', sign_out: 'sign_out', sign_up: 'sign_up' }
+
   root 'home#index'
-
-  # AccessControl controller routes
-  get '/login', to: 'access_control/login#login', as: 'login'
-  post '/login', to: 'access_control/login#create'
-  get '/register', to: 'access_control/register#register', as: 'register'
-  post '/register', to: 'access_control/register#create'
-  delete '/logout', to: 'access_control/login#destroy', as: 'logout'
-
-  # Redirect old routes
-  get '/access_control/login', to: redirect('/login')
-  get '/access_control/register', to: redirect('/register')
 end
